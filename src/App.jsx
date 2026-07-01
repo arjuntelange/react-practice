f7972bd83bf9e6118bb3c4e38ce1874998ebc2b2
Arjun Telange <arjuntelange08@gmail.com>
2026-07-01 22:30:21 +0530

    implement basic routing using BrowserRouter, Routes, Route and Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { Link, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SearchFilter from "./hooks-practice/SearchFilter";
 import PostComment from "./state-projects/comment-post/PostComment";
+import Home from "./routing/student-portal/Home";
+import About from "./routing/student-portal/About";
+import Contact from "./routing/student-portal/Contact";
 
 function App() {
   return (
     <>
       <div className="main-container">
-        <SearchFilter/>
+        <nav>
+          <Link to="/">Home</Link>{" | "}
+          <Link to="/About">About</Link>{" | "}
+          <Link to="/Contact">Contact</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
       </div>
     </>
   );
