7789df2a37df926cf00ba589ba90f1ef12f85167
Arjun Telange <arjuntelange08@gmail.com>
2026-07-12 20:13:51 +0530

    add dynamic payload support using input field

--- src/redux-practice/counter-app/components/Counter.jsx
+++ src/redux-practice/counter-app/components/Counter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   decrement,
@@ -10,13 +10,35 @@ import {
 function Counter() {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+  const [amount, setAmount] = useState("");
+
   return (
     <>
       <h2>count: {count}</h2>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
       <button onClick={() => dispatch(reset())}>Reset</button>
-      <button onClick={() => dispatch(incrementByAmount(10))}>Add 10+</button>
+
+      <input
+        type="text"
+        value={amount}
+        placeholder="Enter amount"
+        onChange={(e) => setAmount(e.target.value)}
+        style={{
+          padding: "10px 16px",
+          border: "none",
+          borderRadius: "14px",
+          fontSize: "15px",
+        }}
+      />
+      <button
+        onClick={() => {
+          dispatch(incrementByAmount(Number(amount)));
+          setAmount("");
+        }}
+      >
+        Add 10+
+      </button>
     </>
   );
 }
