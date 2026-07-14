7b3b8e88404f3a79a4e68433bc230b2b9266f458
Arjun Telange <arjuntelange08@gmail.com>
2026-07-14 19:01:13 +0530

    Install Tailwind CSS and configure project

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Cart from "./redux-practice/shopping-cart/components/Cart";
 function App() {
   return (
     <>
-      <div className="main-container">
+      <div className="w-full h-screen flex bg-orange-100 justify-center items-center gap-8">
         <div className="shopping-container">
           <ProductList />
           <Cart />
